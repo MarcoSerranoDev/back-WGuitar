d999b3c03b6749d9e9e9a31a51a75e994593a274
Andres Cuevas <acg.hermesmusic@gmail.com>
2023-01-17 12:58:36 -0600

    ftr:Agregando estilos a correo babilon

--- src/config/mail.config.js
+++ src/config/mail.config.js
@@ -28,14 +28,23 @@ const sendEmail = async (email, subject, html) => {
 
 const getTemplate = (name, token) => {
   return `
-      <div id="email___content">
-          <h2>Hola ${name}</h2>
-          <p>Para confirmar tu cuenta, ingresa al siguiente enlace</p>
-          <a
-              href="https://back-wguitar-production.up.railway.app/api/v1/user/confirm/${token}"
-              target="_blank"
-          >Confirmar Cuenta</a>
-      </div>
+  <div id="email___content" style="width: 100%; text-align: center;">
+        <h2 style="font-size: 3rem; color: #f9bf19;">Hola ${name}</h2>
+        <div style="text-align: center; line-height: 7px;">
+            <p style="font-size: 1.2rem;">Te has registrado para ganar una guitarra Babilon</p>
+            <p style="font-size: 1.2rem;">Para confirmar tu cuenta, ingresa al siguiente enlace:</p>
+        </div>
+        <a style="background-color: #f9bf19; padding: 10px 20px; border-radius: 10px; color: #fff; text-decoration: none;
+        display: block; font-size: 1.2rem; font-weight: 700; cursor: pointer; width: 200px; margin: 20px auto;"
+            href="https://back-wguitar-production.up.railway.app/api/v1/user/confirm/${token}" target="_blank">Confirmar
+            Cuenta</a>
+        <div style="background: #000; padding: 20px">
+            <img style="width: 400px; height: 200px; object-fit: cover; margin-right: 20px;"
+                src="https://i.ibb.co/sCx2D8z/Babilon-logo-blanco.png" alt="">
+            <img style="width: 200px; height: 200px; margin-left: 20px; object-fit: cover;"
+                src="https://i.ibb.co/tcvcL0d/LOGO-HMC.png" alt="">
+        </div>
+    </div>
     `;
 };
 
